ab3ebf1b73bca77088930d6fbbdb7d30a6e3024b
Ashok Dhungana <you@example.com>
2023-11-04 12:58:12 +0545

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+public class Main{
+    static double add(int x ,int y){
+        return x+y ;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        int a=10, b= 20,  c;
+         c = add(a,b)
+    }
+
+}
